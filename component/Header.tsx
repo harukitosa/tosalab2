387e82a33e0027178c2bf4bbc4adac3997f4b2af
takurinton <takurinton@takurinton.com>
2022-01-29 13:20:22 +0900

    add: a tag in Link component

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,25 +1,27 @@
 import { Box, Flex } from "@chakra-ui/react"
 import Link from 'next/link'
 export function Header(props: HeaderProps) {
-    return (
-      <>
+  return (
+    <>
       <Flex as="nav">
         <Link href={props.url}>
-          <Box 
-            fontWeight="bold" 
-            bg="blue.800" 
-            w="100%" 
-            p={4}
-            pr={6} 
-            color="white" 
-          >
-                tosa.dev
-          </Box>
+          <a>
+            <Box
+              fontWeight="bold"
+              bg="blue.800"
+              w="100%"
+              p={4}
+              pr={6}
+              color="white"
+            >
+              tosa.dev
+            </Box>
+          </a>
         </Link>
       </Flex>
       <Box h="42"></Box>
-      </>
-    );
+    </>
+  );
 }
 
 interface HeaderProps {
